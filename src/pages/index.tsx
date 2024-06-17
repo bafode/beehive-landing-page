04c54eaf23fa79a03ddf50c5bc535d9c3c5ac1ee
camarab <bafode.camara@sacem.fr>
2024-06-17 23:14:20 +0200

    BHIVE-7 setup hero page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,14 @@ import dynamic from 'next/dynamic'
 import { NextPageWithLayout } from '@/interfaces/layout'
 import { MainLayout } from '@/components/layout'
 import { Grid } from '@mui/material'
+const DynamicHomeHero = dynamic(() => import('../screens/Hero'))
 
 const Home: NextPageWithLayout = () => {
   return (
     <Fragment>
       <Grid spacing={2}>
         <Grid item xs={12}>
-          <h1>Beehive</h1>
+          <DynamicHomeHero />
         </Grid>
       </Grid>
     </Fragment>
